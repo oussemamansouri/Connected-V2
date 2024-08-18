668fce5b11bd1eebee621f2f28bf768197e49975
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-19 00:49:46 +0100

    Create registre.component.ts

---
+++ Connected/FrontEnd/src/app/views/front/registre/registre/registre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registre',
+  templateUrl: './registre.component.html',
+  styleUrls: ['./registre.component.scss']
+})
+export class RegistreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
