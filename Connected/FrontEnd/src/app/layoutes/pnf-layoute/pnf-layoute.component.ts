8f0d70ea27eb20a40e6c3fb017c3d559d3d11a7d
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-18 01:48:07 +0100

    Create pnf-layoute.component.ts

---
+++ Connected/FrontEnd/src/app/layoutes/pnf-layoute/pnf-layoute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pnf-layoute',
+  templateUrl: './pnf-layoute.component.html',
+  styleUrls: ['./pnf-layoute.component.scss']
+})
+export class PnfLayouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
