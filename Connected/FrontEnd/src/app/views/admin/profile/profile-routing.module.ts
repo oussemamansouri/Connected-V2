a64660d692aeb8c15c3129a9bdffb716bdf2c93a
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-19 15:35:29 +0100

    Create profile-routing.module.ts

---
+++ Connected/FrontEnd/src/app/views/admin/profile/profile-routing.module.ts
@@ -0,0 +1,13 @@
+import { ProfileComponent } from './../profile/profile/profile.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'',component:ProfileComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule { }
