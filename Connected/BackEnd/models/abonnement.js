a468623f3f00ddb065fed6f4d8ca30b1638481b1
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-25 20:22:09 +0100

    Update abonnement.js

--- Connected/BackEnd/models/abonnement.js
+++ Connected/BackEnd/models/abonnement.js
@@ -1,34 +1,36 @@
-module.exports=(sequelize,DataType)=>{
-    const Abonnement=sequelize.define("Abonnement",{
-        date_debut:{
-            type:DataType.DATE,
-            allowNull:true
+module.exports = (sequelize, DataType) => {
+    // Define the Abonnement model with its attributes and options
+    const Abonnement = sequelize.define("Abonnement", {
+        // Attribute for the start date of the subscription
+        date_debut: {
+            type: DataType.DATE, // Data type is DATE
+            allowNull: true // Field can be null
         },
-        date_fin:{
-            type:DataType.DATE,
-            allowNull:true
+        // Attribute for the end date of the subscription
+        date_fin: {
+            type: DataType.DATE, // Data type is DATE
+            allowNull: true // Field can be null
         },
-        type:{
-            type:DataType.STRING,
-            allowNull:true
-        },
-   
-
+        // Attribute for the type of subscription
+        type: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
+        }
     }, {
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci'
-      })
+        // Options for character set and collation
+        charset: 'utf8mb4', // Character set for the table
+        collate: 'utf8mb4_general_ci' // Collation for sorting and comparing strings
+    });
 
+    // Define associations between models
     Abonnement.associate = models => {
+        // Abonnement model belongs to the Client model
         Abonnement.belongsTo(models.Client, {
-           onDelete: "cascade",
-           onUpdate: 'cascade'
-        })
-      
+            onDelete: "cascade", // If the associated Client is deleted, also delete the Abonnement
+            onUpdate: 'cascade' // If the associated Client is updated, also update the Abonnement
+        });
+    };
 
-
-    }  
-
-return Abonnement
-
-} 
+    // Return the defined Abonnement model
+    return Abonnement;
+};
