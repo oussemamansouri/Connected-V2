d7e45c968aba7a35fa5efc592aa28517036a710a
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-25 20:28:30 +0100

    Update participation.js

--- Connected/BackEnd/models/participation.js
+++ Connected/BackEnd/models/participation.js
@@ -1,23 +1,28 @@
-module.exports=(sequelize,DataType)=>{
-    const Participation=sequelize.define("Participation",{
-       },{
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci'
-      })
+module.exports = (sequelize, DataType) => {
+  // Define the 'Participation' model with an empty attributes object
+  const Participation = sequelize.define("Participation", {}, {
+    charset: 'utf8mb4', // Set the character set to support a wide range of characters, including emojis
+    collate: 'utf8mb4_general_ci' // Set the collation to a case-insensitive, general-purpose collation
+  });
 
-    Participation.associate=models=>{
+  // Define associations with other models
+  Participation.associate = models => {
 
-        Participation.belongsTo(models.Client, {
-            onDelete: "cascade",
-            onUpdate: 'cascade'
-         })
-         Participation.belongsTo(models.Formation, {
-            onDelete: "cascade",
-            onUpdate: 'cascade'
-         })
+    // Establish a many-to-one relationship with the 'Client' model
+    // Each participation record belongs to a single client
+    Participation.belongsTo(models.Client, {
+      onDelete: "cascade", // If a client is deleted, also delete their participation records
+      onUpdate: 'cascade'  // If a client's primary key is updated, cascade the update to the participation records
+    });
 
-    }  
+    // Establish a many-to-one relationship with the 'Formation' model
+    // Each participation record is associated with a single formation
+    Participation.belongsTo(models.Formation, {
+      onDelete: "cascade", // If a formation is deleted, also delete related participation records
+      onUpdate: 'cascade'  // If a formation's primary key is updated, cascade the update to the participation records
+    });
+  };
 
-return Participation
-
-} 
+  // Return the defined model
+  return Participation;
+};
