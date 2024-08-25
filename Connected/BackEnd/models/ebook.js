b25158b51a48680bbaa8a69d2e6ae68c61f33f39
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-25 20:26:44 +0100

    Update ebook.js

--- Connected/BackEnd/models/ebook.js
+++ Connected/BackEnd/models/ebook.js
@@ -1,65 +1,77 @@
-module.exports=(sequelize,DataType)=>{
-    const Ebook=sequelize.define("Ebook",{
-        titre:{
-            type:DataType.STRING,
-            allowNull:true
+module.exports = (sequelize, DataType) => {
+    // Define the Ebook model with its attributes and options
+    const Ebook = sequelize.define("Ebook", {
+        // Attribute for the title of the ebook
+        titre: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-        discription:{
-            type:DataType.STRING,
-            allowNull:true
+        // Attribute for the description of the ebook
+        discription: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-        auteur:{
-            type:DataType.STRING,
-            allowNull:true
+        // Attribute for the author of the ebook
+        auteur: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-        format:{
-            type:DataType.STRING,
-            allowNull:true
+        // Attribute for the format of the ebook (e.g., PDF, EPUB)
+        format: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-        nb_pages:{
-            type:DataType.INTEGER,
-            allowNull:true
+        // Attribute for the number of pages in the ebook
+        nb_pages: {
+            type: DataType.INTEGER, // Data type is INTEGER
+            allowNull: true // Field can be null
         },
-        img:{
-            type:DataType.STRING,
-            allowNull:true
+        // Attribute for the image URL or path representing the ebook cover
+        img: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-        prix:{
-            type:DataType.FLOAT,
-            allowNull:true
+        // Attribute for the price of the ebook
+        prix: {
+            type: DataType.FLOAT, // Data type is FLOAT (for prices with decimals)
+            allowNull: true // Field can be null
         },
-        promotion:{
-            type:DataType.INTEGER,
-            allowNull:true
+        // Attribute for the discount on the ebook price (as a percentage)
+        promotion: {
+            type: DataType.INTEGER, // Data type is INTEGER
+            allowNull: true // Field can be null
         },
-        categorie:{
-            type:DataType.STRING,
-            allowNull:true
+        // Attribute for the category of the ebook
+        categorie: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-        book:{
-            type:DataType.STRING,
-            allowNull:true
+        // Attribute for the ebook file URL or path
+        book: {
+            type: DataType.STRING, // Data type is STRING
+            allowNull: true // Field can be null
         },
-
     }, {
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci'
-      })
+        // Options for character set and collation
+        charset: 'utf8mb4', // Character set for the table
+        collate: 'utf8mb4_general_ci' // Collation for sorting and comparing strings
+    });
 
+    // Define associations between models
     Ebook.associate = models => {
+        // Ebook model belongs to the Centre model
         Ebook.belongsTo(models.Centre, {
-           onDelete: "cascade",
-           onUpdate: 'cascade'
-        })
+            onDelete: "cascade", // If the Centre is deleted, also delete its associated Ebooks
+            onUpdate: 'cascade' // If the Centre is updated, also update its associated Ebooks
+        });
 
-        Ebook.hasMany(models.Achat,{
-            onDelete:"cascade",
-            onUpdate: 'cascade'
-      })
+        // Ebook model has many Achat instances
+        Ebook.hasMany(models.Achat, {
+            onDelete: "cascade", // If the Ebook is deleted, also delete its associated Achats
+            onUpdate: 'cascade' // If the Ebook is updated, also update its associated Achats
+        });
+    };
 
-
-    }  
-
-return Ebook
-
-} 
+    // Return the defined Ebook model
+    return Ebook;
+};
