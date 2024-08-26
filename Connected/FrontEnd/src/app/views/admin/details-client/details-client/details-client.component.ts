86c9e3542b2dc243e2fef1310ef7d616d7a6bec2
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-27 00:26:39 +0100

    Update details-client.component.ts

--- Connected/FrontEnd/src/app/views/admin/details-client/details-client/details-client.component.ts
+++ Connected/FrontEnd/src/app/views/admin/details-client/details-client/details-client.component.ts
@@ -9,65 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsClientComponent implements OnInit {
 
-  clientId:any
-  dataclient:any
-  imagepath:any='http://localhost:3000/'
-  participation:any
-  participants:any=[]
-  achat:any
-  formationid:any
-  formation:any={date_debut:"",date_fin:"",id:"",titre:"",discription:"",img:"",prix:"",heures:"",promotion:"",categorie:"",etat:"",diplome:"",certifiee:"",createdAt:"",updatedAt:"",CentreId:"",Centre:{}}
-  buys:any=[]
-  ebookid:any
-  ebook:any={id:'',titre:'',discription:'',auteur:'',format:'',nb_pages:'',img:'',prix:'',promotion:'',book:'',createdAt:'',updatedAt:'',CentreId:''}
+  clientId: any
+  dataclient: any
+  imagepath: any = 'http://localhost:3000/'
+  participation: any
+  participants: any = []
+  achat: any
+  formationid: any
+  formation: any = { date_debut: "", date_fin: "", id: "", titre: "", discription: "", img: "", prix: "", heures: "", promotion: "", categorie: "", etat: "", diplome: "", certifiee: "", createdAt: "", updatedAt: "", CentreId: "", Centre: {} }
+  buys: any = []
+  ebookid: any
+  ebook: any = { id: '', titre: '', discription: '', auteur: '', format: '', nb_pages: '', img: '', prix: '', promotion: '', book: '', createdAt: '', updatedAt: '', CentreId: '' }
 
-  constructor(private route:ActivatedRoute,private api:ApiService,private router:Router) { }
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
-this.clientId=this.route.snapshot.queryParams['clientId']
-this.api.getclient(this.clientId).subscribe(data=>this.dataclient=data)
-this.api.getclientparticipation(this.clientId).subscribe(data=>this.participation=data)
-this.api.getclientachat(this.clientId).subscribe(data=>this.achat=data)
+    this.clientId = this.route.snapshot.queryParams['clientId']
+    this.api.getclient(this.clientId).subscribe(data => this.dataclient = data)
+    this.api.getclientparticipation(this.clientId).subscribe(data => this.participation = data)
+    this.api.getclientachat(this.clientId).subscribe(data => this.achat = data)
   }
 
-  updateimage(event:any){
+  updateimage(event: any) {
     if (event.target.files.length > 0) {
       const path = event.target.files[0];
       const formData = new FormData();
       formData.append('img', path)
-      this.api.updateclientimage(formData,this.clientId).subscribe(info=>this.ngOnInit())
+      this.api.updateclientimage(formData, this.clientId).subscribe(info => this.ngOnInit())
     }
   }
 
-  sendid2(){
-    this.router.navigate(['/admin/ebook/buys'],{queryParams:{ebookId:this.ebookid}})
+  sendid2() {
+    this.router.navigate(['/admin/ebook/buys'], { queryParams: { ebookId: this.ebookid } })
   }
 
-  getformationid(id:any){
-    this.formationid=id
-    this.api.getformation(id).subscribe(data=>{this.formation=data
-      if (this.formation.certifiee=='true'){
-        this.formation.certifiee='Oui'
-      }else if (this.formation.certifiee=='false'){
-        this.formation.certifiee='Non'
+  getformationid(id: any) {
+    this.formationid = id
+    this.api.getformation(id).subscribe(data => {
+      this.formation = data
+      if (this.formation.certifiee == 'true') {
+        this.formation.certifiee = 'Oui'
+      } else if (this.formation.certifiee == 'false') {
+        this.formation.certifiee = 'Non'
       }
     })
-    this.api.getparticipant(id).subscribe(info=>this.participants=info)
+    this.api.getparticipant(id).subscribe(info => this.participants = info)
   }
 
-  deleteformation(){
-    this.api.deleteparticipation(this.clientId,this.formationid).subscribe(info=>this.ngOnInit())
+  deleteformation() {
+    this.api.deleteparticipation(this.clientId, this.formationid).subscribe(info => this.ngOnInit())
   }
 
-  getbookId(id:any){
-    this.ebookid=id
-this.api.getebookbyid(id).subscribe(info=>{this.ebook=info})
-this.api.getbuyersebook(id).subscribe(data=>this.buys=data)
+  getbookId(id: any) {
+    this.ebookid = id
+    this.api.getebookbyid(id).subscribe(info => { this.ebook = info })
+    this.api.getbuyersebook(id).subscribe(data => this.buys = data)
   }
 
-  deleteebook(){
-    this.api.deleteachatclient(this.clientId,this.ebookid).subscribe(info=>this.ngOnInit())
-      }
+  deleteebook() {
+    this.api.deleteachatclient(this.clientId, this.ebookid).subscribe(info => this.ngOnInit())
+  }
 
   downloadebook() {
     this.api.downloadebook(this.ebookid).subscribe((blob) => {
@@ -84,22 +85,22 @@ this.api.getbuyersebook(id).subscribe(data=>this.buys=data)
     });
   }
 
-  sendformationid(){
-    this.router.navigate(['/admin/formation/update'],{queryParams:{formationId:this.formationid}})
+  sendformationid() {
+    this.router.navigate(['/admin/formation/update'], { queryParams: { formationId: this.formationid } })
   }
-  sendebookid(){
-    this.router.navigate(['/admin/ebook/update'],{queryParams:{ebookId:this.ebookid}})
+  sendebookid() {
+    this.router.navigate(['/admin/ebook/update'], { queryParams: { ebookId: this.ebookid } })
   }
-  sendid(){
-    this.router.navigate(['/admin/formation/participants'],{queryParams:{formationId:this.formationid}})
-      }
+  sendid() {
+    this.router.navigate(['/admin/formation/participants'], { queryParams: { formationId: this.formationid } })
+  }
 
-      sendidclient(){
-        this.router.navigate(['/admin/client/update'],{queryParams:{clientId:this.clientId}})
-      }
-      deleteclient(){
-        this.api.deleteclient(this.clientId).subscribe(info=>this.ngOnInit())
+  sendidclient() {
+    this.router.navigate(['/admin/client/update'], { queryParams: { clientId: this.clientId } })
+  }
+  deleteclient() {
+    this.api.deleteclient(this.clientId).subscribe(info => this.ngOnInit())
 
-      }
+  }
 
 }
