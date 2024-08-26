f38a55c2236118b9e2bd676120ddd9efa47d11d8
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-27 00:26:18 +0100

    Update buys-ebook.component.ts

--- Connected/FrontEnd/src/app/views/admin/buys-ebook/buys-ebook/buys-ebook.component.ts
+++ Connected/FrontEnd/src/app/views/admin/buys-ebook/buys-ebook/buys-ebook.component.ts
@@ -8,43 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buys-ebook.component.scss']
 })
 export class BuysEbookComponent implements OnInit {
-  clients:any
-  clientid:any
-  ebookid:any
-  searchTerm:any
-  constructor(private router:Router,private api:ApiService,private route:ActivatedRoute) { }
+  clients: any;  // Stores the list of clients who bought the eBook
+  clientid: any; // Stores the ID of the selected client
+  ebookid: any;  // Stores the ID of the eBook
+  searchTerm: any; // Stores the search term for filtering clients
+
+  constructor(private router: Router, private api: ApiService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.ebookid=this.route.snapshot.queryParams['ebookId']
-    this.api.getbuyersebook(this.ebookid).subscribe(data=>this.clients=data)
+    // Retrieve the eBook ID from the query parameters
+    this.ebookid = this.route.snapshot.queryParams['ebookId'];
+    
+    // Fetch clients who bought the eBook and assign them to the clients property
+    this.api.getbuyersebook(this.ebookid).subscribe(data => this.clients = data);
   }
 
-  sendId(id:any){
-    this.router.navigate(['/admin/client/details'],{queryParams:{clientId : id}})
-      }
+  // Navigate to client details page with the selected client's ID
+  sendId(id: any) {
+    this.router.navigate(['/admin/client/details'], { queryParams: { clientId: id } });
+  }
 
-      getclientid(id:any){
-        this.clientid=id
-          }
+  // Set the client ID for the selected client
+  getclientid(id: any) {
+    this.clientid = id;
+  }
 
-          deleteachat(){
-            this.api.deleteachat(this.clientid,this.ebookid).subscribe(info=>this.ngOnInit())
-              }
+  // Delete the purchase record for the selected client and eBook
+  deleteachat() {
+    this.api.deleteachat(this.clientid, this.ebookid).subscribe(() => this.ngOnInit());
+  }
 
-              filter() {
-                if (!this.searchTerm) {
-                   return this.clients.rows;
-                }
+  // Filter clients based on the search term
+  filter() {
+    if (!this.searchTerm) {
+      return this.clients.rows; // Return all clients if no search term is provided
+    }
 
-                const filteredClients = this.clients.rows.filter((client: any) => {
-                   const firstNameMatch = client.Client.firstname.toLowerCase().includes(this.searchTerm.toLowerCase());
-                   const lastNameMatch = client.Client.lastname.toLowerCase().includes(this.searchTerm.toLowerCase());
+    // Filter clients based on whether their first name or last name matches the search term
+    const filteredClients = this.clients.rows.filter((client: any) => {
+      const firstNameMatch = client.Client.firstname.toLowerCase().includes(this.searchTerm.toLowerCase());
+      const lastNameMatch = client.Client.lastname.toLowerCase().includes(this.searchTerm.toLowerCase());
 
-                   return firstNameMatch || lastNameMatch;
-                });
+      return firstNameMatch || lastNameMatch;
+    });
 
-                return filteredClients;
-             }
-
-
+    return filteredClients; // Return the filtered list of clients
+  }
 }
