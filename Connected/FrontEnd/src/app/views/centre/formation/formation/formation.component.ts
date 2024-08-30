3cd9c78bc2c2df30067ff26793e8e16fa4808065
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-30 16:57:26 +0100

    Update formation.component.ts

--- Connected/FrontEnd/src/app/views/centre/formation/formation/formation.component.ts
+++ Connected/FrontEnd/src/app/views/centre/formation/formation/formation.component.ts
@@ -1,63 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ApiService } from './../../../../services/crud/api.service';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { Component, OnInit } from '@angular/core'; // Import Angular core component
+import { Router } from '@angular/router'; // Import Router for navigation
+import { ApiService } from './../../../../services/crud/api.service'; // Import custom API service
+import { JwtHelperService } from '@auth0/angular-jwt'; // Import JwtHelperService for decoding JWT tokens
 
 @Component({
-  selector: 'app-formation',
-  templateUrl: './formation.component.html',
-  styleUrls: ['./formation.component.scss']
+  selector: 'app-formation', // Component selector
+  templateUrl: './formation.component.html', // Path to the component's HTML template
+  styleUrls: ['./formation.component.scss'] // Path to the component's stylesheet
 })
 export class FormationComponent implements OnInit {
 
-  searchTerm:any
-  formations:any
-  formation:any={}
-  formationid:any
-  imagepath:any='http://localhost:3000/'
-  participation:any=[]
-hepler = new JwtHelperService
-  constructor(private api:ApiService,private router:Router) { }
+  searchTerm: any; // Search term for filtering formations
+  formations: any; // List of formations
+  formation: any = {}; // Data of the selected formation
+  formationid: any; // ID of the selected formation
+  imagepath: any = 'http://localhost:3000/'; // Base path for images
+  participation: any = []; // List of participants in the selected formation
+  hepler = new JwtHelperService(); // Instance of JwtHelperService
+
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
-let token:any=localStorage.getItem('token')
-let decodedtoken:any=this.hepler.decodeToken(token)
-let centreid=decodedtoken.id
-    this.api.getcentreformations(centreid).subscribe(data=>this.formations=data)
+    // Decode token and fetch formations for the centre
+    let token: any = localStorage.getItem('token');
+    let decodedtoken: any = this.hepler.decodeToken(token);
+    let centreid = decodedtoken.id;
+    this.api.getcentreformations(centreid).subscribe(data => this.formations = data);
   }
 
-  getformationid(id:any){
-    this.formationid=id
-    this.api.getformation(id).subscribe(data=>{this.formation=data
-    if (this.formation.certifiee=='true'){
-      this.formation.certifiee='Oui'
-    }else if (this.formation.certifiee=='false'){
-      this.formation.certifiee='Non'
-    }
-  })
-    this.api.getparticipant(id).subscribe(info=>this.participation=info)
-
-  }
-  deleteformation(){
-    this.api.deleteformation(this.formationid).subscribe(info=>this.ngOnInit())
-  }
-  sendid(){
-    this.router.navigate(['/centre/formation/update'],{queryParams:{formationId:this.formationid}})
+  getformationid(id: any) {
+    // Fetch formation details and participants
+    this.formationid = id;
+    this.api.getformation(id).subscribe(data => {
+      this.formation = data;
+      // Convert certification status to human-readable format
+      if (this.formation.certifiee === 'true') {
+        this.formation.certifiee = 'Oui';
+      } else if (this.formation.certifiee === 'false') {
+        this.formation.certifiee = 'Non';
       }
-      sendformationid(){
-        this.router.navigate(['/centre/formation/participants'],{queryParams:{formationId:this.formationid}})
-          }
+    });
+    this.api.getparticipant(id).subscribe(info => this.participation = info);
+  }
 
+  deleteformation() {
+    // Delete the selected formation and refresh the list
+    this.api.deleteformation(this.formationid).subscribe(() => this.ngOnInit());
+  }
 
-          filter() {
-            if (!this.searchTerm) {
-              return this.formations;
-            }
+  sendid() {
+    // Navigate to formation update page with the formation ID
+    this.router.navigate(['/centre/formation/update'], { queryParams: { formationId: this.formationid } });
+  }
 
-            return this.formations.filter((item:any) =>
-              item.titre.toLowerCase().includes(this.searchTerm.toLowerCase())
-            );
-          }
+  sendformationid() {
+    // Navigate to formation participants page with the formation ID
+    this.router.navigate(['/centre/formation/participants'], { queryParams: { formationId: this.formationid } });
+  }
 
-
+  filter() {
+    // Filter formations based on search term
+    if (!this.searchTerm) {
+      return this.formations;
+    }
+    return this.formations.filter((item: any) =>
+      item.titre.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
